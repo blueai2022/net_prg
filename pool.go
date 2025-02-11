b218f7ceb7291895a17818d7eb54cb49f07caef4
blueai2022 <100088145+blueai2022@users.noreply.github.com>
2025-02-11 11:56:30 -0500

    Update pool.go

--- pool.go
+++ pool.go
@@ -9,7 +9,6 @@ type Task interface {
 }
 
 type Pool struct {
-	// tasks      []Task
 	numThreads int
 	tasksChan  chan Task
 	wg         sync.WaitGroup
@@ -23,7 +22,6 @@ func NewPool(numThreads int) *Pool {
 }
 
 func (pool *Pool) Run() {
-	// start pool number of threads to wait for task on channel
 	for i := 0; i < pool.numThreads; i++ {
 		go pool.runWorker()
 	}
@@ -35,17 +33,14 @@ func (pool *Pool) runWorker() {
 	}
 }
 
-// Wait waits for all submitted tasks to complete
 func (pool *Pool) Wait() {
 	pool.wg.Wait()
 }
 
-// Close closes the task channel, indicating no more tasks will be submitted
 func (pool *Pool) Close() {
 	close(pool.tasksChan)
 }
 
-// Submit adds a new task to the pool
 func (pool *Pool) Submit(task Task) {
 	pool.wg.Add(1)
 	pool.tasksChan <- task
